9437157a2f43ee9963d08e54d275057f8474d732
mlseligson <mseligson125@gmail.com>
2024-08-09 02:49:35 -0500

    Update AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,63 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ViewChild } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NavComponent } from "./nav/nav.component";
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    NavComponent,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  constructor(private auth: AuthService) {
+    
+  }
+  
+  @ViewChild(MatMenuTrigger) trigger!: MatMenuTrigger;
+  
   title = 'vaca';
+  links = [
+    { text: 'Trips', link: 'trips', icon: 'lightbulb' },
+    { text: 'Activities', link: 'activities', icon: 'hiking'},
+    { text: 'Flights', link: 'flights', icon: 'flights_and_hotels'}
+  ];
+  loginProgress = false;
+
+  credentials = {
+    username: '',
+    password: ''
+  };
+
+  attemptLogin() {
+    this.loginProgress = true;
+    this.auth.login(this.credentials).subscribe(res => {
+      this.loginProgress = false;
+      this.trigger.toggleMenu();
+    });
+  }
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TripsComponent } from './trips/trips.component';
+import { ActivitiesComponent } from './activities/activities.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'trips', component: TripsComponent },
+  { path: 'activities', component: ActivitiesComponent }
+];
