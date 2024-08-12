b3a1fc691b5465f0f1da416d76ae7747ca61e17a
mlseligson <mseligson125@gmail.com>
2024-08-12 00:23:00 -0500

    Began building ActivitiesComponent

--- src/app/activities/activities.component.ts
+++ src/app/activities/activities.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { vacation_activities } from './activity-suggestions.json';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-activities',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatListModule, MatDividerModule],
   templateUrl: './activities.component.html',
   styleUrl: './activities.component.scss'
 })
 export class ActivitiesComponent {
-
+  activityList = vacation_activities;
 }
