c62d9bce24e06d5eda9e47b7722ea85cb5769e3b
mlseligson <mseligson125@gmail.com>
2024-08-16 01:05:29 -0500

    Nest list in expansion panels

--- src/app/activities/activities.component.ts
+++ src/app/activities/activities.component.ts
@@ -3,11 +3,17 @@ import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { vacation_activities } from './activity-suggestions.json';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-activities',
   standalone: true,
-  imports: [MatCardModule, MatListModule, MatDividerModule],
+  imports: [
+    MatCardModule,
+    MatListModule,
+    MatDividerModule,
+    MatExpansionModule
+  ],
   templateUrl: './activities.component.html',
   styleUrl: './activities.component.scss'
 })
