7fd69c09de34c6a4dcefdc5c367dec7d5fdbaf28
mlseligson <mseligson125@gmail.com>
2024-08-16 01:04:12 -0500

    Add TripEditComponent route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { TripsComponent } from './trips/trips.component';
 import { ActivitiesComponent } from './activities/activities.component';
+import { TripEditComponent } from './trip-edit/trip-edit.component';
 
 export const routes: Routes = [
   { path: 'trips', component: TripsComponent },
-  { path: 'activities', component: ActivitiesComponent }
+  { path: 'trip/:id', component: TripEditComponent },
+  { path: 'activities', component: ActivitiesComponent },
+  { path: '**', redirectTo: 'trips' }
 ];
