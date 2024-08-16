182c05e87dc5962e2d882d095755ef20e23b7813
mlseligson <mseligson125@gmail.com>
2024-08-16 01:03:34 -0500

    Display trips as cards

--- src/app/trips/trips.component.ts
+++ src/app/trips/trips.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TripService, Trip } from '../services/trip.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-trips',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './trips.component.html',
   styleUrl: './trips.component.scss'
 })
-export class TripsComponent {
+export class TripsComponent implements OnInit {
+  trips: Trip[] = [];
 
+  constructor(
+    private tripService: TripService
+  ) {}
+
+  ngOnInit(): void {
+    this.tripService.indexTrips().subscribe({
+      next: (trips: Trip[]) => {
+        this.trips = trips;
+      },
+      error: () => {}
+    });
+  }
 }
