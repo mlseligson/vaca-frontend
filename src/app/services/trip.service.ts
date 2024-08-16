28fab6fc4d62cf242d4da6fa0e129f750ca8c6ad
mlseligson <mseligson125@gmail.com>
2024-08-16 01:02:12 -0500

    TripService

---
+++ src/app/services/trip.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Trip {
+  id: number;
+  name: string;
+  destination: string;
+  cost: number;
+  user_id: number;
+  image_url: string;
+  start_time: string;
+  end_time: string;
+}
+
+const tripApiUrl = '/api/trips';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TripService {
+
+  constructor(private http: HttpClient) { }
+
+  indexTrips(): Observable<[Trip]> {
+    return this.http.get<[Trip]>(tripApiUrl);
+  }
+
+  getTrip(id: number): Observable<Trip> {
+    return this.http.get<Trip>(`${tripApiUrl}/${id}`);
+  }
+}
