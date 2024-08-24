5616a8094f68eaf821f68a3d83b79c1d67d7ce77
mlseligson <mseligson125@gmail.com>
2024-08-24 00:56:59 -0600

    Added "add to" autocomplete form

--- src/app/activities/activities.component.ts
+++ src/app/activities/activities.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { vacation_activities } from './activity-suggestions.json';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { Trip, TripService } from '../services/trip.service';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-activities',
@@ -12,11 +19,34 @@ import { MatExpansionModule } from '@angular/material/expansion';
     MatCardModule,
     MatListModule,
     MatDividerModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatIconModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   templateUrl: './activities.component.html',
   styleUrl: './activities.component.scss'
 })
-export class ActivitiesComponent {
+export class ActivitiesComponent implements OnInit {
   activityList = vacation_activities;
+  tripList!: Trip[];
+
+  addTo: FormControl;
+
+  constructor(
+    private tripService: TripService
+  ) {
+    this.addTo = new FormControl('');
+  }
+
+  ngOnInit(): void {
+    this.tripService.indexTrips().subscribe({
+      next: (trips: Trip[]) => {
+        this.tripList = trips;
+      }
+    })
+  }
 }
