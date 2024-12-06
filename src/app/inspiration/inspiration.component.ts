40fb8dab234394fc536ddabbb0ca3449e829b63b
mlseligson <mseligson125@gmail.com>
2024-12-06 00:24:47 -0600

    Update Inspiration to use stepper

--- src/app/inspiration/inspiration.component.ts
+++ src/app/inspiration/inspiration.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, signal, Signal, ViewChild, WritableSignal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule, MatSelectionList } from '@angular/material/list';
 import { vacation_activities as activityList } from '../static-data/activity-suggestions.json';
@@ -7,11 +7,13 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { Trip, VacaApiService } from '../services/vaca-api.service';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectTrigger } from '@angular/material/select';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatChipInputEvent, MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-inspiration',
@@ -27,7 +29,9 @@ import { MatSelectTrigger } from '@angular/material/select';
     MatAutocompleteModule,
     MatFormFieldModule,
     MatInputModule,
-    MatSelectTrigger
+    MatSelectTrigger,
+    MatStepperModule,
+    MatChipsModule
   ],
   templateUrl: './inspiration.component.html',
   styleUrl: './inspiration.component.scss'
@@ -38,11 +42,26 @@ export class InspirationComponent implements OnInit {
   addTo: FormControl;
   @ViewChild('activities') activities!: MatSelectionList;
 
+  tripSelectFormGroup: FormGroup;
+  keywordsFormGroup: FormGroup;
+  keywords: WritableSignal<string[]>;
+
   constructor(
-    private api: VacaApiService
+    private api: VacaApiService,
+    private formBuilder: FormBuilder
   ) {
     this.addTo = new FormControl('');
     this.activityList = activityList;
+
+    this.tripSelectFormGroup = formBuilder.group({
+      trip: this.formBuilder.control('', Validators.required)
+    });
+
+    this.keywordsFormGroup = formBuilder.group({
+      keywords: this.formBuilder.control('')
+    });
+
+    this.keywords = signal([]);
   }
 
   ngOnInit(): void {
@@ -51,6 +70,28 @@ export class InspirationComponent implements OnInit {
         this.tripList = trips;
       }
     })
+  }
+
+  addKeyword(event: MatChipInputEvent): void {
+    const activity = (event.value || '').trim();
+
+    // Add our keyword
+    if (activity && activity.length) {
+      this.keywords.update(k => [...k, activity]);
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+  }
+
+  removeKeyword(keyword: string): void {
+    this.keywords.update(k => {
+      const i = k.indexOf(keyword);
+      if (i >= 0)
+        k.splice(i, 1);
+      
+      return k;
+    });
   }
 
   attemptAddActivities(): void {
