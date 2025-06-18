66327c874245652f8300dc99c91db4dc93985327
mlseligson <mseligson125@gmail.com>
2025-06-18 01:37:56 -0600

    cdkDrag media grid

--- src/app/trip-edit/tabs/media.component.ts
+++ src/app/trip-edit/tabs/media.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NgxMasonryModule } from 'ngx-masonry';
+import { CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
     selector: 'app-media',
-    imports: [NgxMasonryModule],
+    imports: [NgxMasonryModule, CdkDrag, CdkDropList],
     templateUrl: './media.component.html',
     styleUrl: './media.component.scss'
 })
@@ -12,15 +13,19 @@ export class MediaComponent {
     {
       title: "Look at this photograph..",
       img: {
-        src: "/public/images/13.png",
+        src: "/images/13.png",
         alt: "Every time it makes me laugh.."
       }
     }, {
       title: "Fun in the sun",
       img: {
-        src: "/public/images/15.png",
+        src: "/images/15.png",
         alt: "that looks fun"
       }
     }
-  ]
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.items, event.previousIndex, event.currentIndex);
+  }
 }
